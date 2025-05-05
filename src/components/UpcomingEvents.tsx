f89608ee67b3c0890f63ea1bd732ab0e0f76c7a2
haroldinie <haroldinie123@gmail.com>
2025-05-05 16:01:04 +0100

    added conditional logic to not display past date events on upcomingEvents component

--- src/components/UpcomingEvents.tsx
+++ src/components/UpcomingEvents.tsx
@@ -3,6 +3,7 @@ import React from "react";
 type EventType = {
   title: string;
   description: string;
+  date: string;
 };
 
 type UpcomingEventsProps = {
@@ -10,34 +11,42 @@ type UpcomingEventsProps = {
 };
 
 const UpcomingEvents: React.FC<UpcomingEventsProps> = ({ events }) => {
-  return (
-    <div
-      id="upcoming-events-wrapper"
-      className="p-6 flex justify-center items-center"
-    >
-      <div className="max-w-screen-md w-full">
-        <div className="flex items-center justify-between w-full p-5 font-bold rtl:text-right text-[#EAEAEA] border-b-0 rounded-t-xl dark:border-[#EAEAEA] gap-3">
-          <h2 className="text-xl font-bold">Upcoming Events</h2>
-        </div>
+  const now = new Date();
+const filteredEvents = events.filter((event) => new Date(event.date) > now);
 
-        {events.length > 0 ? (
-          events.map((event, index) => (
-            <details
-              key={index}
-              className={`border ${
-                index === 0 ? "rounded-t-xl" : ""
-              } border-[#EAEAEA]`}
-            >
-              <summary className="p-5 font-medium text-[#EAEAEA] cursor-pointer">
-                {event.title}
-              </summary>
-              <div className="p-5 border-t-2 border-[#8E793E] text-[#EAEAEA]">
-                <p>- {event.description}</p>
-              </div>
-            </details>
-          ))
-        ) : (
-          <p>No upcoming events found</p>
+return (
+  <div
+    id="upcoming-events-wrapper"
+    className="p-6 flex justify-center items-center"
+  >
+    <div className="max-w-screen-md w-full">
+      <div className="flex items-center justify-between w-full p-5 font-bold rtl:text-right text-[#EAEAEA] border-b-0 rounded-t-xl dark:border-[#EAEAEA] gap-3">
+        <h2 className="text-xl font-bold">Upcoming Events</h2>
+      </div>
+
+      {filteredEvents.length > 0 ? (
+        filteredEvents.map((event, index) => (
+          <details
+            key={index}
+            className={`border ${
+              index === 0 ? "rounded-t-xl" : ""
+            } border-[#EAEAEA]`}
+          >
+            <summary className="p-5 font-medium text-[#EAEAEA] cursor-pointer">
+              {event.title}
+            </summary>
+            <div className="p-5 border-t-2 border-[#8E793E] text-[#EAEAEA]">
+              <p>- {event.description}</p>
+            </div>
+          </details>
+        ))
+      ) : (
+        
+        <summary className="p-5 font-medium text-[#EAEAEA]">
+        <p className="text-[#EAEAEA] p-5 italic">
+          More events coming soon, please check back later.
+        </p>
+        </summary>
         )}
       </div>
     </div>
